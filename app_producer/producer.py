45f8ed701b7d663a1f33ea811b5da53c3db056b8
achiya reuven <achiya3144@gmail.com>
2025-08-26 17:24:48 +0300

    I added a class to read the messages.

---
+++ app_producer/producer.py
@@ -0,0 +1,23 @@
+from kafka import KafkaProducer
+import json
+
+
+class Producer:
+
+    def __init__(self):
+        self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                                 value_serializer=lambda x:
+                                 json.dumps(x).encode('utf-8'))
+
+
+    def publish_message(self,topic,message):
+        self.producer.send(topic, message)
+        self.producer.flush()
+
+
+
+
+
+
+
+
